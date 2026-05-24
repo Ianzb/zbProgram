2b2a72221e7daef258105e7e226e358a715a2278
Ianzb <93322252@qq.com>
2026-05-24 17:33:32 +0800

    同步zbGuiTemplate 5.7.2。

--- zbProgram/app/program/program.py
+++ zbProgram/app/program/program.py
@@ -36,7 +36,7 @@ class Program:
     VERSION = "5.11.2"  # 程序版本
     VERSION_CODE = 70  # 版本序数
     ADDON_API_VERSION = 9  # 插件版本序数
-    CORE_VERSION = "5.7.1"  # 内核版本
+    CORE_VERSION = "5.7.2"  # 内核版本
     TITLE = f"{NAME} {VERSION}"  # 程序标题
     URL = "https://ianzb.github.io/project/zbProgram.html"  # 程序网址
     LICENSE = "GPLv3"  # 程序许可协议
